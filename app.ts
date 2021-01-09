058273a65edebba759793850211f202fcbb606ec
ryabykhms <ryabykhms@gmail.com>
2021-01-09 18:22:44 +0300

    RC-1 feat(all): change files from js to ts and set eslint

---
+++ app.ts
@@ -0,0 +1,31 @@
+import express, { json, Request, Response, urlencoded } from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+
+import { wordsRouter } from './routes';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.use('/words', wordsRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.json({
+    statusCode: 404,
+  });
+});
+
+// error handler
+app.use((err: Error, req: Request, res: Response) => {
+  res.json({
+    statusCode: 500,
+    message: err.message,
+  });
+});
+
+export default app;
---
+++ bin/www.ts
@@ -0,0 +1,93 @@
+/**
+ * Module dependencies.
+ */
+
+import debug0 from 'debug';
+import * as http from 'http';
+import app from '../app';
+
+const debug = debug0('rsclone-backend:server');
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val: string) {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+server.listen(port);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: NodeJS.ErrnoException) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  let bind = null;
+
+  if (typeof addr === 'string') {
+    bind = `pipe ${addr}`;
+  } else if (addr !== null) {
+    bind = `port ${addr.port}`;
+  }
+
+  debug(`Listening on ${bind}`);
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.on('error', onError);
+server.on('listening', onListening);
