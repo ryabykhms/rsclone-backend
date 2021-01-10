713ae51ac4f5d0f81d704ba2d3e1d04f239d65c0
ryabykhms <ryabykhms@gmail.com>
2021-01-10 13:44:34 +0300

    RC-2 feat(storage): add filesystem storage

--- storage/fs.ts
+++ storage/fs.ts
@@ -1,3 +1,82 @@
-export const getWord = () => {
-  return 'mock';
+import { promises as fsp } from 'fs';
+
+type WordDefinition = {
+  statusCode: number;
+  word: string;
+  definition: string | number;
+  lang: string;
+  error?: string | boolean;
+};
+
+export const getWordDefinition = async (lang: string, word: string) => {
+  const filePath = `./data/dictionary-${lang}.json`;
+
+  const wordDefinition: WordDefinition = {
+    statusCode: 404,
+    word: word,
+    definition: '',
+    lang,
+  };
+
+  try {
+    const contents = await fsp.readFile(filePath, 'utf-8');
+
+    const parsedList = JSON.parse(contents);
+
+    if (parsedList[word]) {
+      wordDefinition.statusCode = 200;
+      wordDefinition.word = word;
+      wordDefinition.definition = parsedList[word];
+    }
+
+    if (parsedList[word] && parsedList[word]['definition']) {
+      wordDefinition.definition = parsedList[word]['definition'];
+    }
+  } catch (e: unknown) {
+    if (!(e instanceof Error)) {
+      throw e;
+    }
+
+    wordDefinition.error = 'Language not found';
+
+    console.warn(`There was error ${e.message}`);
+  }
+
+  return wordDefinition;
+};
+
+export const getWordByLength = async (lang: string, length: number) => {
+  const filePath = `./data/dictionary-${lang}.json`;
+
+  const wordDefinition: WordDefinition = {
+    statusCode: 404,
+    word: '',
+    definition: '',
+    lang,
+  };
+
+  try {
+    const contents = await fsp.readFile(filePath, 'utf-8');
+    const parsedList = JSON.parse(contents);
+
+    const filteredList = Object.keys(parsedList).filter((key) => key.length === length);
+    const randomKey = Math.floor(Math.random() * filteredList.length);
+    const word = filteredList[randomKey];
+
+    if (filteredList.length && word) {
+      wordDefinition.statusCode = 200;
+      wordDefinition.word = word;
+      wordDefinition.definition = parsedList[word]['definition'] || parsedList[word];
+    }
+  } catch (e: unknown) {
+    if (!(e instanceof Error)) {
+      throw e;
+    }
+
+    wordDefinition.error = 'Language not found';
+
+    console.warn(`There was error ${e.message}`);
+  }
+
+  return wordDefinition;
 };
