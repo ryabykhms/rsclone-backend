192cee79de65a113a3a7fcc6a47ef3532a9832c4
ryabykhms <ryabykhms@gmail.com>
2021-01-11 10:50:07 +0300

    RC-7 feat(app,wordsRouter): change words routes

--- routes/words.ts
+++ routes/words.ts
@@ -5,12 +5,22 @@ export const router = Router();
 
 router.get('/:lang/:word', async (req, res, next) => {
   const { lang, word } = req.params;
-  const wordDefinition = await storage.getWordDefinition(lang, word);
-  res.json(wordDefinition);
+  const wordInfo = await storage.mongo.getWordInfo(lang, word);
+  res.json(wordInfo);
 });
 
 router.get('/:lang/length/:length', async (req, res, next) => {
   const { lang, length } = req.params;
-  const wordDefinition = await storage.getWordByLength(lang, parseInt(length));
-  res.json(wordDefinition);
+  const wordInfo = await storage.mongo.getWordByLength(lang, parseInt(length));
+  res.json(wordInfo);
+});
+
+router.post('/:lang/:word', async (req, res, next) => {
+  const { lang, word } = req.params;
+  let definition = '';
+  if (req.body['definition']) {
+    definition = req.body.definition;
+  }
+  const result = await storage.mongo.addWord(lang, word, definition);
+  res.json(result);
 });
