3d4be1c18d11f757d8bf0212f857e239da243b62
ryabykhms <ryabykhms@gmail.com>
2021-02-03 12:38:04 +0300

    RC-5 feat(models/user): change user model

--- models/User.ts
+++ models/User.ts
@@ -2,7 +2,8 @@ import { Schema, model } from 'mongoose';
 
 const schema = new Schema({
   login: { type: String, required: true, unique: true },
-  password: { type: String, requird: true },
+  password: { type: String, required: true },
+  score: { type: Number, required: false, default: 0 },
 });
 
 export default model('User', schema);
