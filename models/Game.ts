8ec6ce743a40bc361b380ad9ed729bba7c70a79f
ryabykhms <ryabykhms@gmail.com>
2021-02-03 12:39:12 +0300

    RC-5 feat(models/game): add game model

---
+++ models/Game.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from 'mongoose';
+
+const schema = new Schema({
+  userId: { type: String, required: true },
+  isBot: { type: Boolean, required: true, default: false },
+  fieldSize: { type: Number, required: true, default: 5 },
+  score: { type: Number, required: true, default: 0 },
+  time: { type: Number, required: true, default: 0 },
+  isWin: { type: Boolean, required: true, default: false },
+});
+
+export default model('Game', schema);
