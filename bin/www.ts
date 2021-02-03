f788b0e04e10ef0ce6c5a6b004490b5698deacf4
ryabykhms <ryabykhms@gmail.com>
2021-02-03 12:38:35 +0300

    RC-5 feat(www): change default port

--- bin/www.ts
+++ bin/www.ts
@@ -35,7 +35,7 @@ function normalizePort(val: string) {
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '5000');
 app.set('port', port);
 
 /**
